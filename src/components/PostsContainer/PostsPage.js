3aa7d57170b00969400566dea0c123a9a4b5a38c
Shawn Elston <elstonshawn1@yahoo.com>
2020-02-19 07:07:47 -0800

    still working on it

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,27 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+
+import React, { useState } from 'react';
 import Post from "./Post";
 import "./Posts.css";
+import dummyData from "../../dummy-data";
 // import data 
 
 const PostsPage = () => {
   // set up state for your data
+  console.log(dummyData)
+  const [data, setData] = useState(dummyData);
+  
   return (
+    
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {data.map((post, index ) => {
+        return (
+          <Post key={index} post={post}/>
+        )
+      })}
+    
     </div>
   );
 };
